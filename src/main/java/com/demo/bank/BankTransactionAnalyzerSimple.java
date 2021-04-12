2a38748d12fa678c59b3ed23c9458c00efb07311
dodeon <sojeongw.official@gmail.com>
2021-04-12 16:12:01 +0900

    2-1. 모든 거래 내역의 합 계산하기

---
+++ src/main/java/com/demo/bank/BankTransactionAnalyzerSimple.java
@@ -0,0 +1,30 @@
+package com.demo.bank;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class BankTransactionAnalyzerSimple {
+
+  private static final String RESOURCES = "src/main/resources/";
+
+  public static void main(String[] args) throws IOException {
+    final String fileName = "bank-data-simple.csv";
+    final Path path = Paths.get(RESOURCES + fileName);
+    final List<String> lines = Files.readAllLines(path);
+
+    double total = 0d;
+    for (final String line : lines) {
+      final String[] columns = line.split(",");
+      final double amount = Double.parseDouble(columns[1]);
+
+      total += amount;
+    }
+
+    System.out.println("total = " + total);
+
+
+  }
+}
