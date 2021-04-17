e9eb41c7c035811446205b55fc0444153ae1aa12
dodeon <sojeongw.official@gmail.com>
2021-04-17 18:42:54 +0900

    2-14. CSV 파서의 유닛 테스트가 실패하는 경우

---
+++ src/test/java/com/demo/bank/BankStatementCSVParserTest.java
@@ -0,0 +1,17 @@
+package com.demo.bank;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class BankStatementCSVParserTest {
+
+  private final BankStatementParser statementParser = new BankStatementCSVParser();
+
+  @Test
+  public void shouldParseOneCorrectLine() throws Exception {
+    Assertions.fail("Not yet implemented");
+  }
+
+}
