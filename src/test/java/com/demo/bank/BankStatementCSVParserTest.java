7de2132d84d7637fbfeb9da8b8fb023ef0295049
dodeon <sojeongw.official@gmail.com>
2021-04-17 18:46:27 +0900

    2-15. Assertion 구문 사용하기

--- src/test/java/com/demo/bank/BankStatementCSVParserTest.java
+++ src/test/java/com/demo/bank/BankStatementCSVParserTest.java
@@ -2,6 +2,8 @@ package com.demo.bank;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.LocalDate;
+import java.time.Month;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -11,7 +13,14 @@ class BankStatementCSVParserTest {
 
   @Test
   public void shouldParseOneCorrectLine() throws Exception {
-    Assertions.fail("Not yet implemented");
+    final String line = "30-01-2017,-50,Tesco";
+    final BankTransaction result = statementParser.parseFrom(line);
+
+    final BankTransaction expected = new BankTransaction(LocalDate.of(2017, Month.JANUARY, 30), -50, "Tesco");
+
+    Assertions.assertEquals(expected.getDate(), result.getDate());
+    Assertions.assertEquals(expected.getAmount(), result.getAmount());
+    Assertions.assertEquals(expected.getDescription(), result.getDescription());
   }
 
 }
