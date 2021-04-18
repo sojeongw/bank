20a9dee248259ee87bb2520325ffe5a3596bba2e
dodeon <sojeongw.official@gmail.com>
2021-04-18 19:14:50 +0900

    3-9. 갓 인터페이스

---
+++ src/main/java/com/demo/bank/BankTransactionProcessor.java
@@ -0,0 +1,19 @@
+package com.demo.bank;
+
+import java.time.Month;
+import java.util.List;
+
+public interface BankTransactionProcessor {
+
+  double calculateTotalAmount();
+
+  double calculateTotalInMonth(Month month);
+
+  double calculateTotalInJanuary();
+
+  double calculateAverageAmount();
+
+  double calculateAverageAmountForCategory(Category category);
+
+  List<BankTransaction> findTransactions(BankTransactionFilter bankTransactionFilter);
+}
