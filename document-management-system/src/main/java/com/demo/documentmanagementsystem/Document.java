ca51329b47b0b4790729770952fe1da66e9c7762
dodeon <sojeongw.official@gmail.com>
2021-04-24 20:23:19 +0900

    4-2. Importer 인터페이스

---
+++ document-management-system/src/main/java/com/demo/documentmanagementsystem/Document.java
@@ -0,0 +1,5 @@
+package com.demo.documentmanagementsystem;
+
+public class Document {
+
+}
---
+++ document-management-system/src/main/java/com/demo/documentmanagementsystem/Importer.java
@@ -0,0 +1,8 @@
+package com.demo.documentmanagementsystem;
+
+import java.io.File;
+import java.io.IOException;
+
+public interface Importer {
+  Document importFile(File file) throws IOException;
+}
