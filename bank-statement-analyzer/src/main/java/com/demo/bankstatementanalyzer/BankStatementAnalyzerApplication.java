92b9421e9b2a620b70dca26909526557b19aea4c
dodeon <sojeongw.official@gmail.com>
2021-04-24 19:25:18 +0900

    feat: 새로운 모듈 추가

---
+++ bank-statement-analyzer/src/main/java/com/demo/bankstatementanalyzer/BankStatementAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.demo.bankstatementanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankStatementAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankStatementAnalyzerApplication.class, args);
+	}
+
+}
---
+++ bank-statement-analyzer/src/test/java/com/demo/bankstatementanalyzer/BankStatementAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.bankstatementanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankStatementAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
