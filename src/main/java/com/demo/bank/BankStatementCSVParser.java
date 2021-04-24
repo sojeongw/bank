fb81c6c8bee614ed8cfab5ce6fe7a5c46b15e781
dodeon <sojeongw.official@gmail.com>
2021-04-24 12:58:05 +0900

    3-17. 문법 예외 던지기

--- src/main/java/com/demo/bank/BankStatementCSVParser.java
+++ src/main/java/com/demo/bank/BankStatementCSVParser.java
@@ -8,10 +8,15 @@ import java.util.List;
 public class BankStatementCSVParser implements BankStatementParser {
 
   private static final DateTimeFormatter DATE_PATTERN = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+  private static final int EXPECTED_ATTRIBUTES_LENGTH = 5;
 
   @Override
   public BankTransaction parseFrom(String line) {
     final String[] columns = line.split(",");
+
+    if(columns.length < EXPECTED_ATTRIBUTES_LENGTH) {
+      throw new CSVSyntaxException();
+    }
 
     final LocalDate date = LocalDate.parse(columns[0], DATE_PATTERN);
     final double amount = Double.parseDouble(columns[1]);
