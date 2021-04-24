e4917972c89178bbf8d3d45a9674ddbbf7ef7afb
dodeon <sojeongw.official@gmail.com>
2021-04-24 20:54:00 +0900

    4-5. 자바에서 상수를 정의하는 방법

---
+++ document-management-system/src/main/java/com/demo/documentmanagementsystem/Attributes.java
@@ -0,0 +1,8 @@
+package com.demo.documentmanagementsystem;
+
+public class Attributes {
+  public static final String PATH = "path";
+  public static final String WIDTH = "width";
+  public static final String HEIGHT = "height";
+  public static final String TYPE = "type";
+}
