6b52d261abea3c7e927e58eea63c89e6c8cb9a7d
dodeon <sojeongw.official@gmail.com>
2021-04-24 20:40:05 +0900

    4-3. Document

--- document-management-system/src/main/java/com/demo/documentmanagementsystem/Document.java
+++ document-management-system/src/main/java/com/demo/documentmanagementsystem/Document.java
@@ -1,5 +1,16 @@
 package com.demo.documentmanagementsystem;
 
+import java.util.Map;
+
 public class Document {
 
+  private final Map<String, String> attributes;
+
+  public Document(final Map<String, String> attributes) {
+    this.attributes = attributes;
+  }
+
+  public String getAttribute(final String attributeName) {
+    return attributes.get(attributeName);
+  }
 }
