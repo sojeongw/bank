46c45fd4ac1cd8942690f563814428db86a2e3b9
dodeon <sojeongw.official@gmail.com>
2021-04-24 21:00:25 +0900

    4-6. 임포터 등록

---
+++ document-management-system/src/main/java/com/demo/documentmanagementsystem/DocumentManagementSystem.java
@@ -0,0 +1,14 @@
+package com.demo.documentmanagementsystem;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DocumentManagementSystem {
+  private final Map<String, Importer> extensionToImporter = new HashMap<>();
+
+  public DocumentManagementSystem() {
+    extensionToImporter.put("letter", new LetterImporter());
+    extensionToImporter.put("report", new ReportImporter());
+    extensionToImporter.put("jpg", new ImageImporter());
+  }
+}
---
+++ document-management-system/src/main/java/com/demo/documentmanagementsystem/LetterImporter.java
@@ -0,0 +1,12 @@
+package com.demo.documentmanagementsystem;
+
+import java.io.File;
+import java.io.IOException;
+
+public class LetterImporter implements Importer {
+
+  @Override
+  public Document importFile(File file) throws IOException {
+    return null;
+  }
+}
