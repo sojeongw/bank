f94fd46bd416d8746410790457aefcca02e017d9
dodeon <sojeongw.official@gmail.com>
2021-04-25 14:52:21 +0900

    4-12. TextFile 정의

--- document-management-system/src/main/java/com/demo/documentmanagementsystem/TextFile.java
+++ document-management-system/src/main/java/com/demo/documentmanagementsystem/TextFile.java
@@ -11,6 +11,14 @@ import java.util.List;
 import java.util.Map;
 
 public class TextFile {
+  private final Map<String, String> attributes;
+  private final List<String> lines;
+
+  TextFile(final File file) throws IOException {
+    attributes = new HashMap<>();
+    attributes.put(PATH, file.getPath());
+    lines = Files.lines(file.toPath()).collect(toList());
+  }
 
   void addLineSuffix(final String prefix, final String attributeName) {
     for(final String line: lines) {
