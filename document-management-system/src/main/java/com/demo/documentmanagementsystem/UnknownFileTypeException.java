480f15c374c2661dad579f4b1ea120a2aed6272d
dodeon <sojeongw.official@gmail.com>
2021-04-25 14:20:57 +0900

    4-7. importFile 정의

---
+++ document-management-system/src/main/java/com/demo/documentmanagementsystem/UnknownFileTypeException.java
@@ -0,0 +1,7 @@
+package com.demo.documentmanagementsystem;
+
+public class UnknownFileTypeException extends RuntimeException {
+  public UnknownFileTypeException(final String message) {
+    super(message);
+  }
+}
