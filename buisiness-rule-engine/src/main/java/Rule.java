5c691c043590bd30dc355d25d291bf880e1aa558
dodeon <sojeongw.official@gmail.com>
2021-05-02 14:26:40 +0900

    5-27. 규칙의 개념 모델링

---
+++ buisiness-rule-engine/src/main/java/Rule.java
@@ -0,0 +1,21 @@
+@FunctionalInterface
+interface Rule {
+  void perform(Facts facts);
+}
+
+class DefaultRule implements Rule {
+  private final Condition condition;
+  private final Action action;
+
+  public DefaultRule(final Condition condition, final Action action) {
+    this.condition = condition;
+    this.action = action;
+  }
+
+  @Override
+  public void perform(Facts facts) {
+    if(condition.evaluate(facts)) {
+      action.execute(facts);
+    }
+  }
+}
