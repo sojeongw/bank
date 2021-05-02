8faf374a68f992b6d987f993daa41f7b56f77e12
dodeon <sojeongw.official@gmail.com>
2021-05-02 20:27:28 +0900

    onFollow 추가

--- twooter/src/main/java/Twootr.java
+++ twooter/src/main/java/Twootr.java
@@ -5,4 +5,8 @@ public class Twootr {
   public Optional<SenderEndPoint> onLogon(String userId, final String password, ReceiverEndPoint receiver) {
     return null;
   }
+
+  FollowStatus onFollow(final User follow, final String userIdToFollow) {
+    return null;
+  }
 }
