b4ab3759e020245feb76806972d3ca5feff6fee4
dodeon <sojeongw.official@gmail.com>
2021-05-02 11:06:13 +0900

    5-12. Facts를 이용한 BusinessRuleEngine

--- buisiness-rule-engine/src/main/java/BusinessRuleEngine.java
+++ buisiness-rule-engine/src/main/java/BusinessRuleEngine.java
@@ -4,9 +4,11 @@ import java.util.List;
 public class BusinessRuleEngine {
 
   private final List<Action> actions;
+  private final Facts facts;
 
-  public BusinessRuleEngine() {
+  public BusinessRuleEngine(final Facts facts) {
     this.actions = new ArrayList<>();
+    this.facts = facts;
   }
 
   public void addAction(final Action action) {
@@ -18,7 +20,7 @@ public class BusinessRuleEngine {
   }
 
   public void run() {
-    this.actions.forEach(Action::execute);
+    this.actions.forEach(action -> action.execute(facts));
   }
 
 }
