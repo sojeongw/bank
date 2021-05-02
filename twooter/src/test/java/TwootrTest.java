acaf62e6d7fa3ee446a70d8f698b207a852f6ac1
dodeon <sojeongw.official@gmail.com>
2021-05-02 17:03:26 +0900

    6-1. shouldBeAbleToAuthenticateUser() 뼈대 코드

---
+++ twooter/src/test/java/TwootrTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+public class TwootrTest {
+  @Test
+  public void shouldBeAbleToAuthenticateUser() {
+    // 유효 사용자의 로그온 메시지 수신
+
+    // 로그온 메서드는 새 엔드포인트 반환
+
+    // 엔드포인트 유요성을 확인하는 어서션
+  }
+
+}
