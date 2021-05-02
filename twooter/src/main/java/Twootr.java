fa4930d0ebb4d49ef59162f71ce53c0fc6652cdd
dodeon <sojeongw.official@gmail.com>
2021-05-02 19:06:46 +0900

    6-2. 두번째 onLogon 시그니처

--- twooter/src/main/java/Twootr.java
+++ twooter/src/main/java/Twootr.java
@@ -2,7 +2,7 @@ import java.util.Optional;
 
 public class Twootr {
 
-  public SenderEndPoint onLogon(String userId, final String password, ReceiverEndPoint receiver) {
+  public Optional<SenderEndPoint> onLogon(String userId, final String password, ReceiverEndPoint receiver) {
     return null;
   }
 }
