78eeb8e11926ae9231962e4dda3ebb98c7b645ea
dodeon <sojeongw.official@gmail.com>
2021-05-09 11:37:51 +0900

    6-17. Position

--- twooter/src/main/java/Position.java
+++ twooter/src/main/java/Position.java
@@ -1,3 +1,47 @@
-public class Position {
+import java.util.Objects;
 
+public class Position {
+/**
+ * 최근 확인한 트웃의 위치
+ */
+
+  public static final Position INITIAL_POSITION = new Position(-1);
+
+  private final int value;
+
+  private Position(final int value) {
+    this.value = value;
+  }
+
+  public int getValue() {
+    return value;
+  }
+
+  public Position next() {
+    return new Position(value + 1);
+  }
+
+  @Override
+  public String toString() {
+    return "Position{" +
+        "value=" + value +
+        '}';
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Position position = (Position) o;
+    return getValue() == position.getValue();
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(getValue());
+  }
 }
